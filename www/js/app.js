b57a28dc553ad8bf71c2939c6d11f604a8adcf26
Dmitry <vildhet@gmail.com>
2017-03-05 23:58:52 +0300

    Added second graph

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,30 @@
 const nv = require('nvd3');
 const d3 = require('d3');
 const $ = require('jquery');
+const async = require('async');
 
 window.onload = function() {
-    $.get('data/eurrub', drawCharts);
+    async.parallel([
+        next => $.get('data/brent', data => {
+            next(null, {
+                values: data,
+                key: 'Brent',
+                color: '#ff7f0e'
+            });
+        }),
+        next => $.get('data/eurrub', data => {
+            next(null, {
+                values: data,
+                key: 'Eur/Rub',
+                color: '#2ca02c'
+            });
+        })
+    ], function(error, results) {
+        drawCharts(results);
+    });
 };
 
-function drawCharts(values) {
-    var data = [
-        {
-            values: values,
-            key: 'Eur/Rub'
-        }
-    ];
-
+function drawCharts(data) {
     nv.addGraph(() => {
         var chart = nv.models.lineChart().useInteractiveGuideline(true);
 
@@ -21,9 +32,7 @@ function drawCharts(values) {
             return d3.time.format('%x')(new Date(d * 1000));
         });
 
-        d3.select('#chart svg')
-            .datum(data)
-            .call(chart);
+        d3.select('#chart svg').datum(data).call(chart);
 
         nv.utils.windowResize(chart.update);
         return chart;
